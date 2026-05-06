5870aec52445ea7a5ff74837a766d49b5031da98
Daniela Ortiz <odanny250@gmail.com>
2026-05-05 21:50:35 -0600

    Commit  correcion de direccion en dasboard.php para agregar la funcionalidad de las cookies

--- dashboard.php
+++ dashboard.php
@@ -2,8 +2,12 @@
 session_start();
 
 if (!isset($_SESSION['id_usuario'])) {
+if (isset($_COOKIE['id_usuario'])) {
+    $_SESSION['id_usuario'] = $_COOKIE["id_usuario"];
+  } else {
     header("Location: index.php");
     exit();
+}
 }
 ?>
 <!doctype html>
