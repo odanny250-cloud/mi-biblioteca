9c306bf9e5f24b3da5c620ed51c4135e88da43e7
Daniela Ortiz <odanny250@gmail.com>
2026-05-05 21:57:27 -0600

    Commit  se agrego la funcion de eliminar las cookies para cerrar sesion

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
 session_start();
 session_unset();    // Elimina las variables
 session_destroy();  // Destruye la sesión
+setcookie("id_usuario", "", time() - 3600, "/");
 header("Location: index.php");
 exit();
 ?>
